dea6ec49c9b388b66555a5bcc288155868493306
olia92 <olia.tsm@gmail.com>
2024-04-09 12:22:59 +0300

    serial version

---
+++ serial_code/layers.c
@@ -0,0 +1,287 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+
+
+#include "layers.h"
+
+
+
+//W: input Width, H: input height, D: input depth, K: filter width and height
+// M: output feature maps, S: stride, P: zerro padding
+Conv_Layer * make_conv_layer(int W, int H, int D,int K, int M, int S, int P){
+    
+    Conv_Layer *layer=malloc(sizeof(Conv_Layer));
+
+    layer->in_width=W;
+    layer->in_height=H;
+    layer->in_depth=D;
+
+    layer->filter_width=K;
+    layer->filter_height=K;
+    layer->num_filters=M;
+
+    layer->stride =S;
+    layer->padding=P;
+
+    layer->out_width=(W-K+2*P)/S+1;
+    layer->out_height=(H-K+2*P)/S+1;
+    layer->out_depth=M;
+
+    layer->weights=malloc(sizeof(float)*K*K*M*D);
+    for(int i=0;i<(K*K*M*D);i++){
+        layer->weights[i]=0.f;
+    }
+
+    layer->bias=malloc(sizeof(float)*M);
+    for(int i=0;i<(M);i++){
+        layer->bias[i]=0.f;
+    }
+    
+    return layer;
+}
+
+void conv_forward(float* restrict X,Conv_Layer * l,float* restrict Y) {
+    
+    for(int m=0;m<l->out_depth;m++){
+        int x_j=-l->padding;
+        for(int j=0;j<l->out_height;j++, x_j+=l->stride){
+            int x_i=-l->padding;
+            for(int i=0;i<l->out_width;i++,x_i+=l->stride){
+                int y_idx=i+(l->out_width*(j+m*l->out_height));
+                // Y[y_idx]=0.0f;
+                float sum = 0.0f;
+                for(int c=0;c<l->in_depth;c++){
+                    for(int f_j=0;f_j<l->filter_height;f_j++){
+                        for(int f_i=0;f_i<l->filter_width;f_i++){
+                            int f_idx=f_i+(f_j*l->filter_width)+(c+m*l->in_depth)*(l->filter_height*l->filter_width);
+                            if((x_j+f_j)>=0&&(x_i+f_i)>=0 && (x_j+f_j)<l->in_height&&(x_i+f_i)<l->in_width){
+                                int x_idx= c*l->in_height*l->in_width+(x_j+f_j)*l->in_width+(x_i+f_i);                            
+                                sum+=l->weights[f_idx]*X[x_idx];
+                            }
+                        }
+                    }
+                }
+                sum+=l->bias[m];
+                Y[y_idx]=sum;
+            }
+        }
+    }
+}
+
+ReLU_Layer * make_relu_layer(int W, int H, int D){
+    ReLU_Layer * layer=malloc(sizeof(ReLU_Layer));
+
+    layer->in_width=W;
+    layer->in_height=H;
+    layer->in_depth=D;
+
+    layer->out_width=W;
+    layer->out_height=H;
+    layer->out_depth=D;
+
+    return layer;
+}
+
+void relu_forward(float* restrict X,ReLU_Layer * l,float * restrict Y){
+
+    int size = l->out_depth*l->in_height*l->in_width;
+    for(int i=0;i<size;i++){
+        Y[i]=(X[i]<0.0f) ? 0.0f : X[i];
+    }
+}
+
+Pool_Layer * make_pool_layer(int W, int H, int D,int K, int S, int P){
+    Pool_Layer * layer = malloc(sizeof(Pool_Layer));
+
+    layer->in_width=W;
+    layer->in_height=H;
+    layer->in_depth=D;
+
+    layer->pool_width=K;
+
+    layer->stride=S;
+    layer->padding=P;
+
+    layer->out_width=floor((W-K+2*P)/S+1);
+    layer->out_height=floor((H-K+2*P)/S+1);
+    layer->out_depth=D;
+
+    return layer;
+}
+
+void pool_forward(float * restrict X, Pool_Layer * l,float * restrict Y){
+    for(int m=0;m<l->out_depth;m++){
+        int x_j=-l->padding;
+        for(int j=0;j<l->out_height;x_j+=l->stride,j++){
+            int x_i=-l->padding;
+            for(int i =0; i<l->out_width;x_i+=l->stride,i++){
+                int y_idx = i+l->out_width*(j+m*l->out_height);
+                float max = -INFINITY;
+                for(int p_j=0;p_j<l->pool_width;p_j++){
+                    for(int p_i=0;p_i<l->pool_width;p_i++){
+                        int x_idx = (x_i+p_i) + ((x_j+p_j)+m*l->in_height)*l->in_width;
+                        if((x_i+p_i)>=0&&(x_j+p_j)>=0&&(x_i+p_i)<l->in_width&&(x_j+p_j)<l->in_height){
+                            if(X[x_idx]>max){
+                                max=X[x_idx];
+                            }
+                        }
+                    }
+                }
+                Y[y_idx]=max;
+            }
+        }
+    }
+}
+
+FC_Layer   * make_fc_layer(int W, int H, int D,int num_neurons){
+    FC_Layer * layer = malloc(sizeof(FC_Layer));
+
+    layer->in_neurons = W*H*D;
+    layer->in_width=W;
+    layer->in_height=H;
+    layer->in_depth=D;
+
+    layer->out_width=1;
+    layer->out_height=1;
+    layer->out_depth = num_neurons;
+
+    layer->weights = (float *) malloc(sizeof(float)*layer->in_neurons*layer->out_depth);
+    layer->bias    = (float *) malloc(sizeof(float)*layer->out_depth);
+
+    return layer;
+}
+
+void fc_forward(float * restrict X, FC_Layer * l,float * restrict Y){
+
+    for(int i = 0; i < l->out_depth;i++) {
+            float dot = 0.0f;
+            for(int j = 0; j < l->in_neurons; j++) {
+                int w_idx= j+ i*l->in_neurons;
+                dot += X[j]*l->weights[w_idx];
+            }
+            dot += l->bias[i];
+            Y[i] = dot;
+        }
+
+}
+
+int load_conv(Conv_Layer* l ,char * file_name){
+    
+    int filter_width, filter_height, depth, filters;
+    
+    FILE *fin = fopen(file_name, "r");
+    if (fin == NULL) {
+        printf("Error opening conv_layer file!\n");
+        return 1;
+    }
+
+    fscanf(fin, "%d %d %d %d", &filter_width, &filter_height, &depth, &filters);
+
+    assert(filter_width==l->filter_width);
+    assert(filter_height==l->filter_height);
+    assert(depth==l->in_depth);
+    assert(filters==l->num_filters);
+
+    double val;
+    for(int f = 0; f < filters; f++) {
+        for (int i = 0; i < filter_width; i++) {
+            for (int j = 0; j < filter_height; j++) {
+                for (int d = 0; d < depth; d++) {
+                    fscanf(fin, "%lf", &val);
+                    int idx=i+j*filter_width+(d+f*depth)*(filter_width*filter_height);
+                    l->weights[idx]=(float)val;
+                }
+            }
+        }
+
+    }
+
+    for(int d = 0; d < filters; d++) {
+        fscanf(fin, "%lf", &val);
+        int idx=d;
+        l->bias[idx]=(float)val;
+    }
+
+    fclose(fin);
+    
+    return 0;
+}
+
+int load_fc(FC_Layer *l, const char *filename) {
+    
+    FILE *fin = fopen(filename, "r");
+    if (fin == NULL) {
+        printf("Error opening fc_layer file!\n");
+        return 1;
+    }
+
+    int num_inputs;
+    int output_depth;
+    fscanf(fin, "%d %d", &num_inputs, &output_depth);
+    assert(output_depth == l->out_depth);
+    assert(num_inputs == l->in_neurons);
+
+    double val;
+    for(int i = 0; i < l->out_depth; i++){  
+        for(int h=0;h<l->in_height;h++){
+            for(int w=0;w<l->in_width;w++){
+                for(int d=0;d<l->in_depth;d++){
+                    int idx = w+(h+d*l->in_height)*l->in_width+i*l->in_neurons;
+                    fscanf(fin, "%lf", &val);
+                    l->weights[idx]=(float)val;
+        }}}
+    }
+    for(int i = 0; i < l->out_depth; i++) {
+        fscanf(fin, "%lf", &val);
+        l->bias[i]=(float)val;
+    }
+
+    fclose(fin);
+
+    return 0;
+}
+
+Softmax_Layer * make_softmax_layer(int W, int H, int D){
+
+    Softmax_Layer * layer = malloc(sizeof(Softmax_Layer));
+    layer->in_width = W;
+    layer->in_height = H;
+    layer->in_depth = D;
+
+    layer->out_width = 1;
+    layer->out_height = 1;
+    layer->out_depth = H*W*D;
+
+    layer->likelihoods = (float*) malloc(sizeof(float)*layer->out_depth);
+
+    return layer;
+}
+
+void softmax_forward(float * restrict X, Softmax_Layer * l,float * restrict Y){
+
+    // Compute max activation
+    float amax = X[0]; 
+    for(int i = 1; i < l->out_depth; i++) {
+        if (X[i] > amax) {
+            amax = X[i];
+        }
+    }
+
+    // Compute exponentials and total
+    float total = 0.0f;
+    for(int i = 0; i < l->out_depth; i++) {
+        float e = exp(X[i] - amax);
+        total += e;
+        l->likelihoods[i] = e;
+    }
+
+    // Normalize and output to sum to one
+    for(int i = 0; i < l->out_depth; i++) {
+        Y[i] = l->likelihoods[i] / total;
+
+    }
+
+}
---
+++ serial_code/layers.h
@@ -0,0 +1,139 @@
+#ifndef LAYERS_H
+#define LAYERS_H
+
+#include <stdio.h>
+// Input Data
+#define N_in 32//Input Width-Height
+#define C_in 3//Colour Chanels
+
+//Layer 1 : Convolutional
+#define K1 5 //Filter Width Height
+#define S1 1 //Stride
+#define P1 2 //Padding
+#define M1 16 //Output number of Feature Maps 
+//Layer 2 : ReLU (No parameters)
+//Layer 3 : Pooling
+#define K3 2
+#define S3 2
+#define P3 0
+
+//Layer 4 : Convolutional
+#define K4 5 //Filter Width Height
+#define S4 1 //Stride
+#define P4 2 //Padding
+#define M4 20 //Output number of Feature Maps 
+//Layer 5 : ReLU (No parameters)
+//Layer 6 : Pooling
+#define K6 2
+#define S6 2
+#define P6 0
+
+//Layer 7 : Convolutional
+#define K7 5 //Filter Width Height
+#define S7 1 //Stride
+#define P7 2 //Padding
+#define M7 20 //Output number of Feature Maps 
+//Layer 8 : ReLU (No parameters)
+//Layer 9 : Pooling
+#define K9 2
+#define S9 2
+#define P9 0
+
+//Layer 10 : Fully Connected
+#define M10 10 // Output Neurons
+
+//Layer 11 : Softmax (No parameters)
+
+typedef struct conv_layer {
+    int in_width;
+    int in_height;
+    int in_depth;
+
+    int num_filters;
+    int filter_width;
+    int filter_height;
+    int stride;
+    int padding;
+
+    int out_width;
+    int out_height;
+    int out_depth;
+
+    float *weights;
+    float *bias;
+
+}Conv_Layer;
+
+typedef struct relu_layer {
+    int in_width;
+    int in_height;
+    int in_depth;
+
+    int out_width;
+    int out_height;
+    int out_depth;
+
+}ReLU_Layer;
+
+typedef struct pool_layer {
+    int in_width;
+    int in_height;
+    int in_depth;
+
+    int pool_width;
+    int stride;
+    int padding;
+
+    int out_width;
+    int out_height;
+    int out_depth;
+
+}Pool_Layer;
+
+typedef struct fc_layer {
+    int in_width;
+    int in_height;
+    int in_depth;
+
+    int in_neurons;
+
+    int out_width;
+    int out_height;
+    int out_depth;
+
+    float * weights;
+    float * bias;
+
+}FC_Layer;
+
+typedef struct softmax_layer {
+    int in_width;
+    int in_height;
+    int in_depth;
+
+    int out_width;
+    int out_height;
+    int out_depth;
+
+    float * likelihoods;
+
+}Softmax_Layer;
+
+Conv_Layer * make_conv_layer(int W, int H, int D,int K, int M, int S, int P);
+ReLU_Layer * make_relu_layer(int W, int H, int D);
+Pool_Layer * make_pool_layer(int W, int H, int D,int K, int S, int P);
+FC_Layer   * make_fc_layer(int W, int H, int D,int num_neurons);
+Softmax_Layer * make_softmax_layer(int W, int H, int D);
+
+
+void conv_forward(float* restrict X, Conv_Layer * l,float* restrict Y);
+void relu_forward(float* restrict X, ReLU_Layer * l,float * restrict Y);
+void pool_forward(float * restrict X, Pool_Layer * l,float * restrict Y);
+void fc_forward(float * restrict X, FC_Layer * l,float * restrict Y);
+void softmax_forward(float * restrict X, Softmax_Layer * l,float * restrict Y);
+
+int load_conv(Conv_Layer* l ,char * file_name);
+int load_fc(FC_Layer *l, const char *filename);
+
+
+#endif
