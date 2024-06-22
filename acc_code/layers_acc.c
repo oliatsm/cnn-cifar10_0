84d7fcc588760f271b28f42af7fca3975311c232
olia92 <olia.tsm@gmail.com>
2024-06-22 23:03:40 +0300

    opt #5.0: [Conv_forward] Parallel with gang, collapse(2) worker, reduction vector to the loops.
    In conv_forward use gang, collapse, worker and vector clauses to the loops. Pad_input parallelised too, with collapse(2) on c loop.
    RESULT: About 14.87 times faster than serial code. 25.5 times faster than first version of the code

--- acc_code/layers_acc.c
+++ acc_code/layers_acc.c
@@ -35,9 +35,9 @@ Conv_Layer* make_conv_layer(int W, int H, int D, int K, int M, int S, int P) {
   layer->bias = malloc(sizeof(float) * M);
   layer->in_padded = calloc(layer->padded_size, sizeof(float));
 
-#pragma acc enter data copyin(layer[0:1])
-#pragma acc enter data create(layer->weights[0:layer->weights_size],layer->bias[0:M])
-#pragma acc enter data copyin(layer->in_padded[0:layer->padded_size])
+  #pragma acc enter data copyin(layer[0:1])
+  #pragma acc enter data create(layer->weights[0:layer->weights_size],layer->bias[0:M])
+  #pragma acc enter data copyin(layer->in_padded[0:layer->padded_size])
 
   return layer;
 }
@@ -59,12 +59,12 @@ void free_conv(Conv_Layer* l) {
 // #pragma acc routine
 void pad_input(float* restrict X, Conv_Layer* l) {
 
-    int in_size = l->in_depth * l->in_height * l->in_width;
-  #pragma acc parallel loop copyin(X[0:in_size]) present(l)
+  int in_size = l->in_depth * l->in_height * l->in_width;
+  #pragma acc parallel loop copyin(X[0:in_size]) present(l) collapse(3)
   for (int c = 0; c < l->in_depth; c++) {
-    #pragma acc loop independent
+    // #pragma acc loop  //collapse(2)
     for (int j = 0; j < l->in_height; j++) {
-      #pragma acc loop independent
+      // #pragma acc loop 
       for (int i = 0; i < l->in_width; i++) {
         int padded_idx = (j + l->padding) * l->padded_width + (i + l->padding) + c * l->padded_height * l->padded_width;
         int in_idx = j * l->in_width + i + c * l->in_height * l->in_width;
@@ -83,33 +83,33 @@ void conv_forward(float* restrict X, Conv_Layer* l, float* restrict Y) {
   // #pragma acc data present(l) copyin(X[0:in_size]) copyout(Y[0:l->out_size])
   #pragma acc data present(l) copyout(Y[0:l->out_size])
   {
-  // For each output feature map
-  #pragma acc parallel loop
-  for (int m = 0; m < l->out_depth; m++) {
-    #pragma acc loop independent
-    for (int j = 0; j < l->out_height; j++) {
-      #pragma acc loop independent
-      for (int i = 0; i < l->out_width; i++) {
-        int y_idx = i + (l->out_width * (j + m * l->out_height)); // Output index
-        // Calculate dot product of Weights*Input
-        float sum = 0.0f;
-        #pragma acc loop reduction(+:sum)
-        for (int c = 0; c < l->in_depth; c++) {
-          for (int f_j = 0; f_j < l->filter_width; f_j++) {
-            for (int f_i = 0; f_i < l->filter_width; f_i++) {
-              int f_idx = f_i + (f_j * l->filter_width) + (c + m * l->in_depth) * (l->filter_width * l->filter_width); // Filter Index
-              int x_j = j * l->stride + f_j; // Input height index, increased by stride
-              int x_i = i * l->stride + f_i; // Input width index, increased by stride
-              int x_idx = c * l->padded_height * l->padded_width + x_j * l->padded_width + x_i; // Input index
-              sum += l->weights[f_idx] * l->in_padded[x_idx];
-            } // for f_i
-          } // for f_j
-        } // for c
-        sum += l->bias[m]; // Add bias
-        Y[y_idx] = sum; // Save output result
-      } // for i
-    } // for j
-  } // for m
+    // For each output feature map
+    #pragma acc parallel loop gang
+    for (int m = 0; m < l->out_depth; m++) {
+      #pragma acc loop collapse(2) worker
+      for (int j = 0; j < l->out_height; j++) {
+        // #pragma acc loop 
+        for (int i = 0; i < l->out_width; i++) {
+          int y_idx = i + (l->out_width * (j + m * l->out_height)); // Output index
+          // Calculate dot product of Weights*Input
+          float sum = 0.0f;
+          #pragma acc loop reduction(+:sum) vector
+          for (int c = 0; c < l->in_depth; c++) {
+            for (int f_j = 0; f_j < l->filter_width; f_j++) {
+              for (int f_i = 0; f_i < l->filter_width; f_i++) {
+                int f_idx = f_i + (f_j * l->filter_width) + (c + m * l->in_depth) * (l->filter_width * l->filter_width); // Filter Index
+                int x_j = j * l->stride + f_j; // Input height index, increased by stride
+                int x_i = i * l->stride + f_i; // Input width index, increased by stride
+                int x_idx = c * l->padded_height * l->padded_width + x_j * l->padded_width + x_i; // Input index
+                sum += l->weights[f_idx] * l->in_padded[x_idx];
+              } // for f_i
+            } // for f_j
+          } // for c
+          sum += l->bias[m]; // Add bias
+          Y[y_idx] = sum; // Save output result
+        } // for i
+      } // for j
+    } // for m
   } //acc-data
 }
 
@@ -298,7 +298,7 @@ int load_conv(Conv_Layer* l, char* file_name) {
   }
 
   fclose(fin);
-#pragma acc update device (l->weights[0:l->weights_size],l->bias[0:l->out_depth])
+  #pragma acc update device (l->weights[0:l->weights_size],l->bias[0:l->out_depth])
   return 0;
 }
 
