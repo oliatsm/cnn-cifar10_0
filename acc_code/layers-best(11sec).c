31da88a6ebcc7112bf5b2ae43e2c52dcae053c8c
olia92 <olia.tsm@gmail.com>
2024-07-28 17:55:11 +0300

    layers-best.c: Best loop optimization, time 11,9 - 12,2 seconds

--- acc_code/layers-best(11sec).c
+++ acc_code/layers-best(11sec).c
@@ -1,4 +1,4 @@
-#include "layers_acc.h"
+#include "layers.h"
 
 // Creates a convolutional layer.
 // W: input Width, H: input height, D: input depth, K: filter width and height
@@ -35,9 +35,9 @@ Conv_Layer* make_conv_layer(int W, int H, int D, int K, int M, int S, int P) {
   layer->bias = malloc(sizeof(float) * M);
   layer->in_padded = calloc(layer->padded_size, sizeof(float));
 
-  #pragma acc enter data copyin(layer[0:1])
-  #pragma acc enter data create(layer->weights[0:layer->weights_size],layer->bias[0:M])
-  #pragma acc enter data copyin(layer->in_padded[0:layer->padded_size])
+#pragma acc enter data copyin(layer[0:1])
+#pragma acc enter data create(layer->weights[0:layer->weights_size],layer->bias[0:M])
+#pragma acc enter data copyin(layer->in_padded[0:layer->padded_size])
 
   return layer;
 }
@@ -46,9 +46,9 @@ Conv_Layer* make_conv_layer(int W, int H, int D, int K, int M, int S, int P) {
 // l: Pointer to the convolutional layer to be freed.
 void free_conv(Conv_Layer* l) {
 
-  #pragma acc exit data delete(l->in_padded[0:l->padded_size])
-  #pragma acc exit data delete(l->weights[0:l->weights_size],l->bias[0:l->out_depth])
-  #pragma acc exit data delete(l[0:1])
+#pragma acc exit data delete(l->in_padded[0:l->padded_size])
+#pragma acc exit data delete(l->weights[0:l->weights_size],l->bias[0:l->out_depth])
+#pragma acc exit data delete(l[0:1])
 
   free(l->in_padded);
   free(l->bias);
@@ -58,11 +58,12 @@ void free_conv(Conv_Layer* l) {
 
 // Add zero-padding to input data of conv_layer l
 void pad_input(float* restrict X, Conv_Layer* l) {
-  #pragma acc parallel loop present(X,l)
-  for (int c = 0; c < l->in_depth; c++) {
-    #pragma acc loop 
+
+#pragma acc parallel loop present(l,X)  gang vector_length(32)
+  for ( int c = 0; c < l->in_depth; c++) {
+  #pragma acc loop 
     for (int j = 0; j < l->in_height; j++) {
-      #pragma acc loop 
+      #pragma acc loop vector
       for (int i = 0; i < l->in_width; i++) {
         int padded_idx = (j + l->padding) * l->padded_width + (i + l->padding) + c * l->padded_height * l->padded_width;
         int in_idx = j * l->in_width + i + c * l->in_height * l->in_width;
@@ -77,33 +78,32 @@ void pad_input(float* restrict X, Conv_Layer* l) {
 // X: Input data, l: Convolutional layer, Y: Output data
 void conv_forward(float* restrict X, Conv_Layer* l, float* restrict Y) {
 
-  pad_input(X, l); //Create input with zero-padding
-// For each output feature map
-  #pragma acc parallel loop present(l,Y) gang collapse(2)
-  for (int m = 0; m < l->out_depth; m++) {
-    for (int j = 0; j < l->out_height; j++) {
-      #pragma acc loop worker
-      for (int i = 0; i < l->out_width; i++) {
-        int y_idx = i + (l->out_width * (j + m * l->out_height)); // Output index
-        // Calculate dot product of Weights*Input
-        float sum = 0.0f;
-        #pragma acc loop reduction(+:sum) vector
-        for (int c = 0; c < l->in_depth; c++) {
-          for (int f_j = 0; f_j < l->filter_width; f_j++) {
-            for (int f_i = 0; f_i < l->filter_width; f_i++) {
-              int f_idx = f_i + (f_j * l->filter_width) + (c + m * l->in_depth) * (l->filter_width * l->filter_width); // Filter Index
-              int x_j = j * l->stride + f_j; // Input height index, increased by stride
-              int x_i = i * l->stride + f_i; // Input width index, increased by stride
-              int x_idx = c * l->padded_height * l->padded_width + x_j * l->padded_width + x_i; // Input index
-              sum += l->weights[f_idx] * l->in_padded[x_idx];
-            } // for f_i
-          } // for f_j
-        } // for c
-        sum += l->bias[m]; // Add bias
-        Y[y_idx] = sum; // Save output result
-      } // for i
-    } // for j
-  } // for m
+    pad_input(X, l); //Create input with zero-padding
+    // For each output feature map
+  #pragma acc parallel loop present(l,Y) gang worker collapse(3) vector_length(32)
+    for (int m = 0; m < l->out_depth; m++) {
+      for (int j = 0; j < l->out_height; j++) {
+        for (int i = 0; i < l->out_width; i++) {
+          int y_idx = i + (l->out_width * (j + m * l->out_height)); // Output index
+          // Calculate dot product of Weights*Input
+          float sum = 0.0f;
+        #pragma acc loop reduction(+:sum) vector collapse(2) 
+          for (int c = 0; c < l->in_depth; c++) {
+            for (int f_j = 0; f_j < l->filter_width; f_j++) {
+              for (int f_i = 0; f_i < l->filter_width; f_i++) {
+                int f_idx = f_i + (f_j * l->filter_width) + (c + m * l->in_depth) * (l->filter_width * l->filter_width); // Filter Index
+                int x_j = j * l->stride + f_j; // Input height index, increased by stride
+                int x_i = i * l->stride + f_i; // Input width index, increased by stride
+                int x_idx = c * l->padded_height * l->padded_width + x_j * l->padded_width + x_i; // Input index
+                sum += l->weights[f_idx] * l->in_padded[x_idx];
+              } // for f_i
+            } // for f_j
+          } // for c
+          sum += l->bias[m]; // Add bias
+          Y[y_idx] = sum; // Save output result
+        } // for i
+      } // for j
+    } // for m
 }
 
 // Creates a ReLU activation layer.
@@ -124,7 +124,7 @@ ReLU_Layer* make_relu_layer(int W, int H, int D) {
 
   layer->out_size = layer->out_width * layer->out_height * layer->out_depth;
 
-	// Move ReLU layer data to device memory
+  // Move ReLU layer data to device memory
 	#pragma acc enter data copyin(layer[0:1])
 
   return layer;
@@ -134,7 +134,7 @@ ReLU_Layer* make_relu_layer(int W, int H, int D) {
 // X: Input data, l: ReLU layer, Y: Output data
 void relu_forward(float* restrict X, ReLU_Layer* l, float* restrict Y) {
 
-#pragma acc parallel loop present(l,X,Y) 
+#pragma acc parallel loop present(l,X,Y) gang vector vector_length(32)
   for (int i = 0; i < l->out_size; i++) {
     Y[i] = (X[i] < 0.0f) ? 0.0f : X[i];
   }
@@ -169,7 +169,7 @@ Pool_Layer* make_pool_layer(int W, int H, int D, int K, int S) {
   layer->out_depth = D;
 
   layer->out_size = layer->out_width * layer->out_height * layer->out_depth;
-  
+
   // Move max pooling layer data to device memory
 	#pragma acc enter data copyin(layer[0:1])
 
@@ -180,15 +180,14 @@ Pool_Layer* make_pool_layer(int W, int H, int D, int K, int S) {
 // X: Input data, l: Pooling layer, Y: Output data
 void pool_forward(float* restrict X, Pool_Layer* l, float* restrict Y) {
   // For each output feature map
-  #pragma acc parallel loop present(X,l,Y) gang collapse(2) 
+  #pragma acc parallel loop present(X,l,Y) gang worker collapse(3) vector_length(32)
   for (int m = 0; m < l->out_depth; m++) {
     for (int j = 0; j < l->out_height; j++) {
-      #pragma acc loop worker
       for (int i = 0; i < l->out_width; i++) {
         int y_idx = i + l->out_width * (j + m * l->out_height); // Output index
         // Find Max in pooling filter
         float max = -INFINITY;
-        #pragma acc loop reduction(max:max) vector
+        #pragma acc loop reduction(max:max) vector collapse(2)
         for (int p_j = 0; p_j < l->pool_width; p_j++) {
           for (int p_i = 0; p_i < l->pool_width; p_i++) {
             int x_j = j * l->stride + p_j; // Input height index, increased by stride
@@ -248,13 +247,13 @@ FC_Layer* make_fc_layer(int W, int H, int D, int num_neurons) {
 void fc_forward(float* restrict X, FC_Layer* l, float* restrict Y) {
 
   // For every output neuron
-  #pragma acc parallel loop present(l,X,Y) num_gangs(10) 
+  #pragma acc parallel loop present(l,X,Y) gang worker vector_length(32)
   for (int i = 0; i < l->out_depth; i++) {
     // Calculate dot product of input and weights
     float sum = 0.0f;
-    #pragma loop reduction(+:sum) vector_length(128)
+    #pragma loop reduction(+:sum) vector
     for (int j = 0; j < l->in_neurons; j++) {
-     int w_idx = j + i * l->in_neurons; // Weight index
+      int w_idx = j + i * l->in_neurons; // Weight index
       sum += X[j] * l->weights[w_idx];
     }
     sum += l->bias[i]; // add bias
@@ -319,7 +318,7 @@ int load_conv(Conv_Layer* l, char* file_name) {
   }
 
   fclose(fin);
-  #pragma acc update device (l->weights[0:l->weights_size],l->bias[0:l->out_depth])
+#pragma acc update device (l->weights[0:l->weights_size],l->bias[0:l->out_depth])
   return 0;
 }
 
@@ -411,7 +410,7 @@ void softmax_forward(float* restrict X, Softmax_Layer* l, float* restrict Y) {
   #pragma acc data present(X,l,Y) create(max,total) cache(X[0:10])
   {
   // Compute max activation
-  #pragma acc parallel loop reduction(max:max) num_gangs(1) vector_length(10)
+  #pragma acc parallel loop reduction(max:max) vector vector_length(32)
   for (int i = 0; i < l->out_depth; i++) {
     if (X[i] > max) {
       max = X[i];
@@ -419,7 +418,7 @@ void softmax_forward(float* restrict X, Softmax_Layer* l, float* restrict Y) {
   }
 
   // Compute exponentials and total
-  #pragma acc parallel loop reduction(+:total) num_gangs(1) vector_length(10)
+  #pragma acc parallel loop reduction(+:total) vector vector_length(32)
   for (int i = 0; i < l->out_depth; i++) {
     float e = exp(X[i] - max);
     total += e;
@@ -427,7 +426,7 @@ void softmax_forward(float* restrict X, Softmax_Layer* l, float* restrict Y) {
   }
 
   // Normalize and output to sum to one
-  #pragma acc parallel loop num_gangs(1) vector_length(10)
+  #pragma acc parallel loop vector vector_length(32)
   for (int i = 0; i < l->out_depth; i++) {
     Y[i] = l->likelihoods[i] / total;
   }
