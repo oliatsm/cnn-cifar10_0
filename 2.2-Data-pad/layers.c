b5ddace0d5e26e06b21cc55e539946276851e50f
GeoSko <gskondras@ac.upatras.gr>
2024-12-13 18:51:16 +0200

    Best loop opt

--- 2.2-Data-pad/layers.c
+++ 2.2-Data-pad/layers.c
@@ -82,18 +82,17 @@ void conv_forward(float* restrict X, Conv_Layer* l, float* restrict Y) {
 {
     pad_input(X, l); //Create input with zero-padding
    // For each output feature map
-#pragma acc parallel loop  
+#pragma acc parallel loop gang collapse(3) vector_length(32)
     for ( int m = 0; m < l->out_depth; m++) {
-      #pragma acc loop
       for (int j = 0; j < l->out_height; j++) {
-       #pragma acc loop
+        // #pragma acc loop worker 
         for (int i = 0; i < l->out_width; i++) {
           int y_idx = i + (l->out_width * (j + m * l->out_height)); 
           // Calculate dot product of Weights*Input
           float sum = 0.0f;
-        #pragma acc loop  reduction(+:sum) 
+        #pragma acc loop reduction(+:sum) vector collapse(2)
           for (int c = 0; c < l->in_depth; c++) {
-            for (int f_j = 0; f_j < l->filter_width; f_j++) {
+            for (int f_j = 0; f_j < l->filter_width; f_j++) { 
               for (int f_i = 0; f_i < l->filter_width; f_i++) {
                 int f_idx = f_i + (f_j * l->filter_width) + (c + m * l->in_depth) * (l->filter_width * l->filter_width); 
                 int x_j = j * l->stride + f_j; 
