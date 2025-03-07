16cd71f8418a0897cf4ff3e77be0d31946e4ee03
GeoSko <gskondras@ac.upatras.gr>
2025-03-07 17:11:18 +0200

    Full network paralleliase

--- layers.h
+++ layers.h
@@ -86,13 +86,10 @@ typedef struct conv_layer {
     int out_depth;      // Output depth
 
     int out_size;       // Output volume size
-    int padded_width;
-    int padded_height;
-    int padded_size;
+    int weights_size;   //Weights volume size
 
     float* weights;     // Filter weights
     float* bias;        // Bias values
-    float* padded_input;    // Input with padding
 
 }Conv_Layer;
 
@@ -178,8 +175,6 @@ void relu_forward(float* restrict X, ReLU_Layer* l, float* restrict Y);
 void pool_forward(float* restrict X, Pool_Layer* l, float* restrict Y);
 void fc_forward(float* restrict X, FC_Layer* l, float* restrict Y);
 void softmax_forward(float* restrict X, Softmax_Layer* l, float* restrict Y);
-
-void add_padding(float* restrict X, Conv_Layer* l);
 
 // Functions to load pre-trained data for convolutional and fully connected layers
 int load_conv(Conv_Layer* l, char* file_name);
